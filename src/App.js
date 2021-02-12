f10a3c2d2dee735cd895fa2359a45c3f8bd25b79
andrewsilent <silent.netix@gmail.com>
2021-02-12 15:42:20 +0200

    project UserList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Greeting from './components/Greeting/'
+import UserList from './components/UserList/';
+import SelectedUserList from './components/SelectedUserList';
+
+const userDB = [
+  {
+    id: 1,
+    firstName: "Stan",
+    lastName: "Smith",
+  },
+  {
+    id: 2,
+    firstName: "Francine",
+    lastName: "Smith",
+  },
+  {
+    id: 3,
+    firstName: "Steve",
+    lastName: "Smith",
+  },
+];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: 'John',
-      lastName: 'Smith',
-      url: 'http://localhost:3000/favicon.ico'
+      users: userDB.map((user) => ({ ...user, isSelected: false })),
     }
   }
+
+  setUsers = (newUsers) => this.setState({ users: newUsers });
+
   render() {
-    const { firstName, lastName, url } = this.state;
+    const { users } = this.state;
     return (
-      <div>
-        <Greeting name={firstName} photo={url} />
-        <Greeting name='Andrew' photo={url} sayHello />
-        <Greeting name='Emily' photo={url} sayHello />
-      </div>
+      <>
+        <header>
+          <SelectedUserList users={[...users]} />
+        </header>
+        <main>
+          <UserList users={users} setUsers={this.setUsers} />
+        </main>
+      </>
     )
   }
 }
