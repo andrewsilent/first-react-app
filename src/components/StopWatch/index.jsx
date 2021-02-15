9a821da30461d477668a495deccce8ec89db2292
andrewsilent <silent.netix@gmail.com>
2021-02-15 14:31:58 +0200

    refactor timer on setTimeout

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -3,68 +3,67 @@ import React, { Component } from "react";
 class StopWatch extends Component {
   constructor(props) {
     super(props);
-    this.intervalId = null;
     this.state = {
+      isRunning: false,
+      timerId: null,
       time: new Date(0, 0, 0, 0, 0, 0, 0, 0),
     };
   }
 
   start = () => {
-    if (!this.intervalId) {
-      this.intervalId = setInterval(this.tick, 1000);
-    }
+    this.setState({ isRunning: true });
+  };
+
+  clear = () => {
+    clearTimeout(this.timerId);
+    this.timerId = null;
   };
 
   stop = () => {
-    clearInterval(this.intervalId);
-    this.intervalId = null;
+    this.setState({ isRunning: false });
   };
 
   reset = () => {
     this.stop();
-    this.setState({ time: new Date(0, 0, 0, 0, 0, 0, 0, 0) });
+    this.setState({ time: new Date(0, 0, 0, 0, 0, 0, 0, 0), isRunning: false });
   };
 
   tick = () => {
-    const { time } = this.state;
-    time.setSeconds(time.getSeconds() + 1);
-    this.setState({ time });
+    this.setState((state, props) => {
+      const { time, timerId } = state;
+      const newTime = new Date(time.getTime());
+      newTime.setSeconds(newTime.getSeconds() + 1);
+      return { time: newTime, timerId };
+    });
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    // this.start();
+  }
 
-  componentDidUpdate() {}
+  componentDidUpdate() {
+    const { isRunning } = this.state;
+    this.clear();
+    if (isRunning) {
+      this.timerId = setTimeout(this.tick, 1000);
+    }
+  }
 
   componentWillUnmount() {
     this.stop();
   }
 
   render() {
-    const { time } = this.state;
+    const { time, isRunning } = this.state;
     return (
       <article>
         <h1>{time.toLocaleTimeString("it-IT")}</h1>
-        <button
-          onClick={() => {
-            this.start();
-          }}
-        >
-          Start
-        </button>
-        <button
-          onClick={() => {
-            this.stop();
-          }}
-        >
-          Stop
-        </button>
-        <button
-          onClick={() => {
-            this.reset();
-          }}
-        >
-          Reset
-        </button>
+        {isRunning ? (
+          <button onClick={this.stop}>Stop</button>
+        ) : (
+          <button onClick={this.start}>Start</button>
+        )}
+        <button onClick={this.reset}>Reset</button>
       </article>
     );
   }
