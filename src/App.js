a6eaa289a96aba0e76eb959c29f20a50def0d9e3
andrewsilent <silent.netix@gmail.com>
2021-02-23 20:27:58 +0200

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import Counter from './components/Counter/';
-import UsersLoader from './components/UsersLoader/';
-import WindowSize from './components/WindowSize';
+import UsersLoader from './components/UsersLoader'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter,
+} from 'react-router-dom';
 
-function App (props) {
-  return <WindowSize />;
-}
+const App = props => {
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/contacts'>Contacts</Link>
+          </li>
+          <li>
+            <Link to='/users'>Users</Link>
+          </li>
+        </ul>
+      </nav>
 
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/contacts'>
+          <Contacts />
+        </Route>
+        <Route path='/users'>
+          <UsersLoader />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+const Contacts = () => <div>Contacts</div>;
 export default App;
